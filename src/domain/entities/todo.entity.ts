0aeca5e42d5716e3be2968ab7c1e7f1f91089167
oskrecheverria1996 <oskr.echeverria1996@gmail.com>
2024-08-22 18:05:40 -0500

    @main - rest api with clean architecture

---
+++ src/domain/entities/todo.entity.ts
@@ -0,0 +1,30 @@
+export class TodoEntity {
+
+    constructor(
+        public id: number,
+        public text: string,
+        public completedAt?: Date | null
+    ){}
+
+    get isCompleted() {
+        return !!this.completedAt; // !! --> doble negacion: https://es.stackoverflow.com/questions/125310/qu%C3%A9-es-la-doble-negaci%C3%B3n-en-javascript
+    }
+
+    public static fromObject(object: {[key: string]: any}): TodoEntity {
+        const { id, text, completedAt } = object;
+        if( !id ) throw 'Id is required';
+        if( !text ) throw 'Text is required';
+
+        let newCompletedAt;
+        if (completedAt) {
+            newCompletedAt = new Date(completedAt);
+            if(isNaN(newCompletedAt.getTime())) {
+                throw 'CompletedAt is not a valid date';
+            }
+        }
+
+        return new TodoEntity(id, text, completedAt);
+    }
+
+
+}
