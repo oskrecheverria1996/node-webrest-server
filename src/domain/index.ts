67f99493596761b62ce572971d752a768594ad01
oskrecheverria1996 <oskr.echeverria1996@gmail.com>
2024-08-23 14:01:13 -0500

    @main - adding tests

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,6 +3,7 @@ export * from './dtos';
 export * from './entities/todo.entity';
 export * from './repositories/todo.repository';
 
+export * from './errors/custom.error'
 
 export * from './use-cases/todo/create-todo';
 export * from './use-cases/todo/get-todo';
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
-import { prisma } from "../../data/postgres";
 import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
-import { CreateTodo, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from "../../domain";
+import { CreateTodo, CustomError, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from "../../domain";
 
 export class TodosController {
     
@@ -10,12 +9,22 @@ export class TodosController {
         private readonly todoRepository: TodoRepository
     ) {}
 
+    private handleError = (res: Response, error: unknown) => {
+        if(error instanceof CustomError) {
+            res.status(error.statusCode).json({ error: error.message });
+            return;
+        }
+
+        // grabar log
+        res.status(500).json({ error: 'Internal server error - check logs' });
+    }
+
     public getTodos = (req: Request, res: Response) => {
 
         new GetTodos( this.todoRepository )
         .execute()
         .then(todos => res.json(todos))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => this.handleError(res, error));
     };
 
     public getTodoById = (req: Request, res: Response) => {
@@ -24,7 +33,7 @@ export class TodosController {
         new GetTodo(this.todoRepository)
         .execute( id )
         .then(todo => res.json(todo))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => this.handleError(res, error));
     };
 
     public createTodo = (req: Request, res: Response) => {
@@ -34,8 +43,8 @@ export class TodosController {
 
         new CreateTodo(this.todoRepository)
         .execute(createTodoDto!)
-        .then(todo => res.json(todo))
-        .catch(error => res.status(400).json({error}));
+        .then(todo => res.status(201).json(todo))
+        .catch(error => this.handleError(res, error));
 
     };
 
@@ -48,16 +57,15 @@ export class TodosController {
         new UpdateTodo(this.todoRepository)
         .execute(updateTodoDto!)
         .then(todo => res.json(todo))
-        .catch(error => res.status(400).json({error}))
+        .catch(error => this.handleError(res, error))
     };
 
     public deleteTodo = (req: Request, res: Response) => {
         const id = +req.params.id;
-        const deletedTodo = this.todoRepository.deleteById(id);
         
         new DeleteTodo(this.todoRepository)
         .execute(id)
         .then(todo => res.json(todo))
-        .catch(error => res.status(400).json({error}))
+        .catch(error => this.handleError(res, error))
     }
 }
